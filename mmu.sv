5d0ebb9ae479c968c01bf0f7a4a62d731ce24bbe
Ohad Amsalem <oamsalemd@gmail.com>
2023-08-08 18:04:58 +0300

    Completed softmax, fixed MMU, added "TODO-s"

--- mmu.sv
+++ mmu.sv
@@ -3,9 +3,10 @@
 1. Upon "enable", the MMU will start to multiply the input matrices and accumulate the result in the output matrix.
 2. "enable" must remain asserted until "data_ready" pulses.
 3. First cycle:
-    -> All multiplications occur simultaneously.
+    -> All multiplications occur simultaneously. Saved in FF-s.
    Second cycle:
-    -> All adders are chained together to accumulate the result.
+    -> Mult results (FF-s output) flow into adders.
+       All adders are chained together to accumulate the result.
 4. Then, "data_ready" pulses when the output matrix is ready.
 
 */
@@ -28,9 +29,9 @@ module mmu #(
 );
 
     logic [DATA_WIDTH-1:0] mat_mul_temp [NUM_ROWS_A][NUM_COLS_B][NUM_COLS_A];
-    logic enable_d;
-    logic enable_dd;
+    logic enable_d, enable_dd, enable_ddd;
 
+    // Combo for multiplication:
     generate
         for (genvar i = 0; i < NUM_ROWS_A; i = i + 1) begin: row_a
             for (genvar j = 0; j < NUM_COLS_B; j = j +1) begin: col_b
@@ -53,40 +54,47 @@ module mmu #(
         end
     endgenerate
 
+    // Synchoronous for multiplication:
+    logic [DATA_WIDTH-1:0] mat_mul_temp_reg [NUM_ROWS_A][NUM_COLS_B][NUM_COLS_A];
     always_ff (posedge clk or negedge rst_n) begin
-        generate
-            for (genvar r = 0; r < NUM_ROWS_A; r = r + 1) begin: out_rows
-                for (genvar s = 0; s < NUM_COLS_B; s = s + 1) begin: out_cols
-                    logic [DATA_WIDTH-1:0] adders_chain_result [NUM_COLS_A];
-                    for (genvar t = 1; t < NUM_COLS_A; t = t + 1) begin: out_cols
-                        adder #(DATA_WIDTH, FIXED_PNT) adder_inst (
-                            .num1(adders_chain_result[t-1]),
-                            .num2(mat_mul_temp[r][s][t]),
-                            .sum(adders_chain_result[t]),
-                            .overflow(),
-                            .underflow()
-                        );
-                        end
+        if (~rst_n) mat_mul_temp <= 'h0;
+        else if (enable_d) mat_mul_temp_reg <= mat_mul_temp;
+    end
 
+    // Per output cell:
+    generate
+        for (genvar r = 0; r < NUM_ROWS_A; r = r + 1) begin: out_rows
+            for (genvar s = 0; s < NUM_COLS_B; s = s + 1) begin: out_cols
+                // Combo for chain adders:
+                logic [DATA_WIDTH-1:0] adders_chain_result [NUM_COLS_A];
+                adder #(DATA_WIDTH, FIXED_PNT) adder_inst (
+                    .num1(mat_in_accum[r][s]),
+                    .num2(mat_mul_temp_reg[r][s][0]),
+                    .sum(first_adder_result),
+                    .overflow(),
+                    .underflow()
+                );
+
+                assign adders_chain_result[0] = first_adder_result;
+                for (genvar t = 1; t < NUM_COLS_A; t = t + 1) begin: out_cols
+                    adder #(DATA_WIDTH, FIXED_PNT) adder_inst (
+                        .num1(adders_chain_result[t-1]),
+                        .num2(mat_mul_temp_reg[r][s][t]),
+                        .sum(adders_chain_result[t]),
+                        .overflow(),
+                        .underflow()
+                    );
+                end
+
+                logic [DATA_WIDTH-1:0] first_adder_result;
+                // Synchronous for chain adders:
+                always_ff (posedge clk or negedge rst_n) begin
                     if (~rst_n) mat_out[r][s] <= 'h0;
-                    else if (enable_d) begin
-                        logic [DATA_WIDTH-1:0] first_adder_result;
-                        adder #(DATA_WIDTH, FIXED_PNT) adder_inst (
-                            .num1(mat_in_accum[r][s]),
-                            .num2(mat_mul_temp[r][s][0]),
-                            .sum(first_adder_result),
-                            .overflow(),
-                            .underflow()
-                        );
-
-                        assign adders_chain_result[0] = first_adder_result;
-
-                        mat_out[r][s] <= adders_chain_result[NUM_COLS_A-1]
-                    end
+                    else if (enable_dd)  mat_out[r][s] <= adders_chain_result[NUM_COLS_A-1];
                 end
             end
-        endgenerate
-    end
+        end
+    endgenerate
 
     always_ff (posedge clk or negedge rst_n) begin
         if (~rst_n) enable_d <= 0;
@@ -98,6 +106,11 @@ module mmu #(
         else enable_dd <= enable_d;
     end
 
-    assign data_ready = enable_d & ~enable_dd;
+    always_ff (posedge clk or negedge rst_n) begin
+        if (~rst_n) enable_ddd <= 0;
+        else enable_ddd <= enable_dd;
+    end
+
+    assign data_ready = enable_dd & ~enable_ddd;
 
 endmodule
--- softmax.sv
+++ softmax.sv
@@ -1,6 +1,15 @@
 /*
 
-
+1. Upon "enable" signal, calculate the softmax of the input vector.
+2. "enable" must remain asserted until "data_ready" pulses.
+3. First cycle:
+    -> Calculate the exponential of each element in the input vector.
+   Second cycle:
+    -> Calculate the sum of the exponential values.
+        // TODO: Break down the sum into sub-chains.
+   Third cycle:
+    -> Divide each exponential value by the sum.
+4. Then, 4. Then, "data_ready" pulses when the output vector is ready.
 
 */
 
@@ -17,6 +26,9 @@ module softmax #(
     output logic signed [DATA_WIDTH-1:0] vec_out [VEC_SIZE]
     );
 
+    logic enable_d, enable_dd, enable_ddd;
+
+    // Combo for exp vec:
     logic signed [DATA_WIDTH-1:0] vec_in_exp [VEC_SIZE];
     generate
         for (genvar i = 0; i < VEC_SIZE; i = i + 1) begin: softmax
@@ -24,31 +36,60 @@ module softmax #(
         end
     endgenerate
 
-    logic signed [DATA_WIDTH+$clog2(VEC_SIZE)-1:0] vec_in_exp_sum;
-    generate
-        logic [DATA_WIDTH-1:0] adders_chain_result [VEC_SIZE];
+    // Synchronous for exp vec:
+    logic signed [DATA_WIDTH-1:0] vec_in_exp_reg [VEC_SIZE];
+    always_ff @(posedge clk or negedge rst_n) begin: softmax
+        if (!rst_n) begin
+            for (genvar i = 0; i < VEC_SIZE; i = i + 1) begin
+                vec_in_exp_reg[i] <= 'h0;
+            end
+        end else if (enable_d) begin
+            for (genvar i = 0; i < VEC_SIZE; i = i + 1) begin
+                vec_in_exp_reg[i] <= vec_in_exp[i];
+            end
+        end
+    end
 
-        assign adders_chain_result[0] = vec_in_exp[0];
+    // Combo for adders:
+    logic [DATA_WIDTH-1:0] adders_chain_result [VEC_SIZE];
+
+    generate
+        assign adders_chain_result[0] = vec_in_exp_reg[0];
         for (genvar t = 1; t < VEC_SIZE; t = t + 1) begin: vec_adders_chain
             adder #(DATA_WIDTH, FIXED_PNT) adder_inst (
                 .num1(adders_chain_result[t-1]),
-                .num2(vec_in_exp[t]),
+                .num2(vec_in_exp_reg[t]),
                 .sum(adders_chain_result[t]),
                 .overflow(),
                 .underflow()
             );
         end
-        assign vec_in_exp_sum = adders_chain_result[VEC_SIZE-1];
     endgenerate
 
+    // Synchronous for adders:
+    logic signed [DATA_WIDTH+$clog2(VEC_SIZE)-1:0] vec_in_exp_sum;
+    always_ff @(posedge clk or negedge rst_n) begin: softmax
+        if (!rst_n) vec_in_exp_sum <= 'h0;
+        else if (enable_dd) vec_in_exp_sum <= adders_chain_result[VEC_SIZE-1];
+    end
+
+    // Combo for division:
+    logic signed [DATA_WIDTH-1:0] vec_div [VEC_SIZE];
+    generate
+        for (genvar i = 0; i < VEC_SIZE; i = i + 1) begin: softmax
+            assign vec_div[i] = vec_in_exp_reg[i] / vec_in_exp_sum;
+        end
+    endgenerate
+
+    // Synchronous for division:
     always_ff @(posedge clk or negedge rst_n) begin: softmax
         if (!rst_n) begin
             for (genvar i = 0; i < VEC_SIZE; i = i + 1) begin
                 vec_out[i] <= 'h0;
             end
-        end else if (enable) begin
+        end else if (enable_ddd) begin
             for (genvar i = 0; i < VEC_SIZE; i = i + 1) begin
-                vec_out[i] <= enable ? vec_in_exp[i] / vec_in_exp_sum : 'h0;
+                vec_out[i] <= vec_div[i];
             end
         end
     end
@@ -56,7 +97,21 @@ module softmax #(
     always_ff @(posedge clk or negedge rst_n) begin: softmax
         if (!rst_n) enable_d <= 1'b0;
         else enable_d <= enable;
-        end
     end
 
-    assign data_valid = enable & ~enable_d;
+    always_ff @(posedge clk or negedge rst_n) begin: softmax
+        if (!rst_n) enable_dd <= 1'b0;
+        else enable_dd <= enable_d;
+    end
+
+    always_ff @(posedge clk or negedge rst_n) begin: softmax
+        if (!rst_n) enable_ddd <= 1'b0;
+        else enable_ddd <= enable_dd;
+    end
+
+    always_ff @(posedge clk or negedge rst_n) begin: softmax
+        if (!rst_n) data_valid <= 1'b0;
+        else enable_dddd <= enable_ddd;
+    end
+
+    assign data_valid = enable_ddd & ~enable_dddd;
